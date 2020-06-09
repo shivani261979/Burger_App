55231e37f2fbe006cf5b3e2c87a5f1f06a957d4f
shivani jha <shivani261979@gmail.com>
2020-06-08 23:59:55 -0400

    added config for sql on heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,26 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+
+// mysql://bc48b0fe56007b:13aa3a16@us-cdbr-east-05.cleardb.net/heroku_f98466972273d45?reconnect=true
+
+
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 8889,
+  port: 3306,
   user: "root",
   password: "rootpass",
   database: "burgers_db"
 });
+
+var connectionHeroku = mysql.createConnection({
+  host: "us-cdbr-east-05.cleardb.net",
+  port: 3306,
+  user: "bc48b0fe56007b",
+  password: "13aa3a16",
+  database: "heroku_f98466972273d45"
+});
+
 
 // Make connection.
 connection.connect(function (err) {
@@ -20,3 +33,4 @@ connection.connect(function (err) {
 
 // Export connection for our ORM to use.
 module.exports = connection;
+module.exports = connectionHeroku;
