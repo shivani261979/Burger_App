0c3886552bdf7eba237bbf78162d262a96930260
shivani jha <shivani261979@gmail.com>
2020-06-08 23:12:17 -0400

    added route for favicon.ico

--- server.js
+++ server.js
@@ -20,7 +20,10 @@ app.set("view engine", "handlebars");
 // Import routes and give the server access to them.
 var routes = require("./controllers/burgerController.js");
 
+app.get('/favicon.ico', (req, res) => res.end())
+
 app.use(routes);
+
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function () {
